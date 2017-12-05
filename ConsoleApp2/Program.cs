5f3ad31a4787afef8cf20c22ee0baf5605ab8018
Box-Bop <Box-Bop@github.com>
2017-12-05 09:10:22 +0200

    backup 1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp2
 {
@@ -10,6 +11,22 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Tere!\nMis te soovite teha?");
+            Console.WriteLine("\nLuua uue konto (1)\nLogida sisse (2)");
+            string decision = Console.ReadLine();
+
+            if (decision == "1")
+            {
+                Console.WriteLine("Mis on teie nimi?");
+                var user1 = new User();
+                user1.UserName = Console.ReadLine();
+                Console.WriteLine("Tore!\nNüüd tehke endale parool: ");
+                user1.Psw = Convert.ToString(Console.ReadLine());
+                user1.LoggedIn();
+
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ ConsoleApp2/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    public class User
+    {
+        public string Psw { get; set; }
+        public int Balance { get; set; }
+        public string UserName { get; set; }
+        public string filePath = Directory.GetCurrentDirectory();
+
+        public void LoggedIn()
+        {
+            File.Create(filePath + UserName + ".txt");
+            using (StreamWriter sw = File.CreateText(Path.Combine(filePath + UserName + ".txt")))
+            {
+                sw.WriteLine(Psw);
+            }
+        }
+    }
+}
