45075c7ea5eb44f804b0bb43b83d065c4c41acb6
Box-Bop <Box-Bop@github.com>
2017-12-06 20:37:56 +0200

    Backup 4

---
+++ ConsoleApp2/LogIn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class LogIn : User
+    {
+        public string LogName { get; set; }
+        public int LogPsw { get; set; }
+
+        public void Login()
+        {
+            string fullPath = filePath + "\\" + UserName;
+            File.OpenRead (fullPath);
+            File.
+        }
+
+    }
+}
